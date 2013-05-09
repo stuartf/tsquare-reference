f4a9f1adbaea68f0cbfff1b558332531593b934d
matthew.buckett@it.ox.ac.uk <matthew.buckett@it.ox.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-05-09 14:30:32 +0000

    SAK-22424 merge r110843 from trunk
    git-svn-id: https://source.sakaiproject.org/svn/reference/branches/sakai-2.8.x@123891 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- library/src/webapp/editor/ckeditor.launch.js
+++ library/src/webapp/editor/ckeditor.launch.js
@@ -46,7 +46,7 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config) {
         skin: 'v2',
 		defaultLanguage: 'en',
 		language: language + (country ? '-' + country.toLowerCase() : ''),
-        height: 460,
+        height: 310,
         filebrowserBrowseUrl :'/library/editor/FCKeditor/editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/web/editor/filemanager/browser/default/connectors/jsp/connector' + collectionId + folder,
         filebrowserImageBrowseUrl : '/library/editor/FCKeditor/editor/filemanager/browser/default/browser.html?Type=Image&Connector=/sakai-fck-connector/web/editor/filemanager/browser/default/connectors/jsp/connector' + collectionId + folder,
         filebrowserFlashBrowseUrl :'/library/editor/FCKeditor/editor/filemanager/browser/default/browser.html?Type=Flash&Connector=/sakai-fck-connector/web/editor/filemanager/browser/default/connectors/jsp/connector' + collectionId + folder,
